35dc3bb8d75a15b67e50aa53bb174d33d375d69a
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-30 20:59:21 +0800

    Update info-rules.js

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -17,15 +17,13 @@ let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
 ⟬Mɪɴɪᴍᴀʟ Mᴀɴᴅɪ Wᴀᴊɪʙ Dᴜʟᴜ Bʀᴏ Bɪᴀʀ Tᴀᴜ Dɪᴍᴀɴᴀ Lᴇᴛᴀᴋ Kᴇsᴀʟᴀʜᴀɴ Lᴜ⟭
 ➯ *Cᴀʟʟ AᴜᴛᴏBʟᴏᴄᴋ*
 ⟬Mᴀᴍᴘᴜs Lᴜ⟭
-➯ *Mᴀᴋsᴀ Mɪɴᴛᴀ Sᴄʀɪᴘᴛ Bᴏᴛ*
-⟬Eᴍᴀɴɢ Nʏᴀ Lᴜ Sᴀᴘᴀ? Uᴅʜ Mɪɴᴛᴀ Mᴀᴋsᴀ Pᴜʟᴀ⟭
 ➯ *Hɪɴᴀ Bᴏᴛ Gᴡ*
 ⟬Nɢᴇʜɪɴᴀ Eʟɪᴛ,Bᴜᴋᴛɪ Sᴜʟɪᴛ⟭
 ➯ *Bot Delay/Lama Bles?*
 ⟬Lu Di Chet Ayang Lu Aja Lama Blesny⟭
 ➯ *Maksa Masukin Gc Lu*
 ⟬Minimal Donasi Lah Cuy Biar Cuan Gw⟭
-➯ *Chet Aneh² Di Cwe Gw, Gw Block Lu Hdup²*
+➯ *Chet Aneh² gw spam call lu pake termux*
 ⟬Mɪɴɪᴍᴀʟ Nɢᴏᴛᴀᴋ⟭
 ❏┳━━◩
 ┍┛
@@ -33,13 +31,13 @@ let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
 └─┈⟅`
 let nth = `☰⟥⟝⟞⟝❨ *Rᴜʟᴇs Mʏ Bᴏᴛ* ❩⟞⟝⟞⟤☰`
 conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    mediaUrl: 'https://wa.me/p/5489299044451325/62895327999292',
     mediaType: 2, 
     description: sgc,
     title: "Sᴇᴡᴀ Cᴇᴋ Dɪsɪɴɪ Cᴜʏ!!!",
     body: wm,
     thumbnail: fs.readFileSync('./media/qr.png'),
-    sourceUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    sourceUrl: 'https://wa.me/p/5489299044451325/62895327999292',
      }}
   })
 }
